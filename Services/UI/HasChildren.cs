73d61fc1548f27cfc86c2cd4b21e8351db351dce
Shlizer <hinc.krzysiek@gmail.com>
2017-10-03 22:40:07 +0200

    ui manager

---
+++ Services/UI/HasChildren.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Battleships.Services.UI
+{
+    class HasChildren
+    {
+        protected List<IElement> children;
+
+        public void AddChild(IElement child)
+        {
+            children.Add(child);
+        }
+    }
+}
---
+++ Services/UI/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Battleships.Services.UI
+{
+    class UIManager : HasChildren
+    {
+        private static GraphicsDevice _graphicsDevice;
+        private static ContentManager _content;
+        
+        public UIManager(GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            _graphicsDevice = graphicsDevice;
+            _content = content;
+        }
+
+        public static GraphicsDevice getGraphicsDevice()
+        {
+            return _graphicsDevice;
+        }
+
+        public static ContentManager getContent()
+        {
+            return _content;
+        }
+    }
+}
