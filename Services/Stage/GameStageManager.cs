fec8d4fcc6a3961853ed8cc6cd28e09f2d9b2c7b
Shlizer <hinc.krzysiek@gmail.com>
2017-10-03 22:39:39 +0200

    zmiany w gameStage

---
+++ Services/Stage/GameStageManager.cs
@@ -0,0 +1,65 @@
+﻿using Battleships.Services.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Battleships.Services.Stage
+{
+    class GameStageManager
+    {
+        private Splash stageSplash;
+        private Menu stageMenu;
+        
+        private GameStage _gameStage;
+        private GraphicsDevice _graphicsDevice;
+        private ContentManager _content;
+        private UIManager _uiManager;
+
+        public GameStageManager(GraphicsDevice graphicsDevice, ContentManager content, UIManager uiManager)
+        {
+            _uiManager = uiManager;
+            _graphicsDevice = graphicsDevice;
+            _content = content;
+
+            stageSplash = new Splash(graphicsDevice, content, uiManager);
+            stageMenu = new Menu(graphicsDevice, content, uiManager);
+
+            _gameStage = GameStage.Splash;
+        }
+
+        public string GetStage()
+        {
+            return _gameStage.ToString();
+        }
+
+        public bool Update(GameTime gameTime)
+        {
+            switch (_gameStage)
+            {
+                case GameStage.Splash:
+                    _gameStage = stageSplash.Update(gameTime);
+                    break;
+                case GameStage.Menu:
+                    _gameStage = stageMenu.Update(gameTime);
+                    break;
+                case GameStage.Exit:
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            switch (_gameStage)
+            {
+                case GameStage.Splash:
+                    stageSplash.Draw(gameTime);
+                    break;
+                case GameStage.Menu:
+                    stageMenu.Draw(gameTime);
+                    break;
+            }
+        }
+    }
+}
---
+++ Services/Stage/Menu.cs
@@ -0,0 +1,47 @@
+﻿using Battleships.Services;
+using Battleships.Services.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Battleships.Services.Stage
+{
+    class Menu : BaseStage
+    {
+        private SpriteBatch spriteBatch;
+        private Texture2D loadingScreenBG;
+        private Dialog dialog;
+        private float splashAlpha = 1.0f;
+
+        public Menu(GraphicsDevice graphicsDevice, ContentManager content, UIManager uiManager) : base(graphicsDevice, content, uiManager) {}
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(_graphicsDevice);
+            dialog = new Dialog();
+            loadingScreenBG = _content.Load<Texture2D>("images/loading");
+        }
+
+        public override GameStage Update(GameTime gameTime)
+        {
+            if (splashAlpha >= 0.5f)
+            {
+                splashAlpha -= 0.01f;
+            }
+            return GameStage.Menu;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _graphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+            spriteBatch.Draw(loadingScreenBG, new Rectangle(0, 0, _graphicsDevice.Viewport.Width, _graphicsDevice.Viewport.Height), Color.White * splashAlpha);
+            spriteBatch.End();
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+            dialog.Draw();
+            spriteBatch.End();
+        }
+    }
+}
